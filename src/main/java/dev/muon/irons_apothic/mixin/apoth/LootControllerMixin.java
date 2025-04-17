26def4da9ba9a16cdff70a695ba92f2cebfa382b
muon-rw <pashah012@gmail.com>
2025-04-16 22:58:28 -0500

    More bonuses, update affix filtering to allow non-mainhand elemental sorting

--- src/main/java/dev/muon/irons_apothic/mixin/apoth/LootControllerMixin.java
+++ src/main/java/dev/muon/irons_apothic/mixin/apoth/LootControllerMixin.java
@@ -5,14 +5,21 @@ import dev.muon.irons_apothic.affix.AffixSchoolMapper;
 import dev.shadowsoffire.apotheosis.affix.Affix;
 import dev.shadowsoffire.apotheosis.affix.AffixRegistry;
 import dev.shadowsoffire.apotheosis.affix.AffixType;
+import dev.shadowsoffire.apotheosis.loot.LootCategory;
 import dev.shadowsoffire.apotheosis.loot.LootController;
 import dev.shadowsoffire.apotheosis.loot.LootRarity;
+import dev.shadowsoffire.apothic_attributes.modifiers.VanillaEquipmentSlot;
 import dev.shadowsoffire.placebo.reload.DynamicHolder;
+import dev.shadowsoffire.apothic_attributes.modifiers.EntityEquipmentSlot;
+import dev.shadowsoffire.apothic_attributes.modifiers.EntitySlotGroup;
 import io.redspace.ironsspellbooks.api.spells.SchoolType;
+import net.minecraft.core.Holder;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -20,7 +27,21 @@ import java.util.stream.Stream;
 public class LootControllerMixin {
     @ModifyReturnValue(method = "getAvailableAffixes", at = @At("RETURN"))
     private static Stream<DynamicHolder<Affix>> filterAffixes(Stream<DynamicHolder<Affix>> originalAffixes, ItemStack stack, LootRarity rarity, AffixType type) {
-        Set<SchoolType> gearSpellSchools = AffixSchoolMapper.getSpellSchoolsFromWeapon(stack);
+        LootCategory cat = LootCategory.forItem(stack);
+        EquipmentSlot relevantSlot = EquipmentSlot.MAINHAND;
+
+        EntitySlotGroup group = cat.getSlots();
+        if (group != null) {
+            Optional<Holder<EntityEquipmentSlot>> firstHolder = group.slots().stream().findFirst();
+            if (firstHolder.isPresent()) {
+                EntityEquipmentSlot entitySlot = firstHolder.get().value();
+                if (entitySlot instanceof VanillaEquipmentSlot vanillaWrapper) {
+                    relevantSlot = vanillaWrapper.slot();
+                }
+            }
+        }
+
+        Set<SchoolType> gearSpellSchools = AffixSchoolMapper.getSpellSchoolsFromGear(stack, relevantSlot);
 
         return originalAffixes
                 .filter(a -> {
@@ -32,6 +53,7 @@ public class LootControllerMixin {
                         if (affixSpellSchool != null) {
                             return gearSpellSchools.contains(affixSpellSchool);
                         }
+                        return false;
                     }
                     if (AffixSchoolMapper.isGenericSpellAffix(affixId)) {
                         return gearSpellSchools.isEmpty();
